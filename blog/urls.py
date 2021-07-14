d9790475a2583942b2f6002f154075291b620259
Dat Dao Man <dat.daoman@grevo.net>
2021-07-14 22:07:22 +0700

    remove hard-coding url

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('', views.list),
-  path('<int:id>/', views.detail),
+  path('', views.list, name = 'blog'),
+  path('<int:id>/', views.detail, name = 'post'),
 ]
